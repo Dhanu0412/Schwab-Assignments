93f9c18dd28409dbd93cea7d4e9fa7b9f76a990d
Dhanu0412 <dhanushya0412@gmail.com>
2021-11-29 11:11:31 +0530

    Deadlock, Fibonacci Series, Minimum String Length and Reassemble

--- Deadlock.java
+++ Deadlock.java
@@ -5,63 +5,69 @@
  * least two threads and two or more threads. Let’s write a simple program and 
  * execute/run the code to create a deadlock.
  */
-class Resource{
-  private final String name;
-  public Resource(String name){
-    this.name = name;
-  }
-  public String getName() {
-    return this.name;
-  }
+class Resource {
+	private final String name;
+
+	public Resource(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 }
 
-class ThreadA implements Runnable{
-  private Resource r1;
-  private Resource r2;
-  ThreadA(Resource r1, Resource r2){
-    this.r1 = r1;
-    this.r2 = r2;
-  }
-  @Override
-  public void run() {
-    synchronized(r1){
-      System.out.println("" + r1.getName() + " locked by Thread A");
-      synchronized(r2){
-        System.out.println("" + r2.getName() + " locked by Thread A");
-      }
-    }      
-  }  
+class ThreadA implements Runnable {
+	private Resource r1;
+	private Resource r2;
+
+	ThreadA(Resource r1, Resource r2) {
+		this.r1 = r1;
+		this.r2 = r2;
+	}
+
+	@Override
+	public void run() {
+		synchronized (r1) {
+			System.out.println("" + r1.getName() + " locked by Thread A");
+			synchronized (r2) {
+				System.out.println("" + r2.getName() + " locked by Thread A");
+			}
+		}
+	}
 }
 
-class ThreadB extends Thread{
-  private Resource r1;
-  private Resource r2;
-  ThreadB(Resource r1, Resource r2){
-    this.r1 = r1;
-    this.r2 = r2;
-  }
-  @Override
-  public void run() {
-    synchronized(r2){
-      System.out.println("" + r2.getName() + " locked by Thread B");
-      synchronized(r1){
-        System.out.println("" + r1.getName() + " locked by Thread B");
-      }
-    }   
-  }
+class ThreadB extends Thread {
+	private Resource r1;
+	private Resource r2;
+
+	ThreadB(Resource r1, Resource r2) {
+		this.r1 = r1;
+		this.r2 = r2;
+	}
+
+	@Override
+	public void run() {
+		synchronized (r2) {
+			System.out.println("" + r2.getName() + " locked by Thread B");
+			synchronized (r1) {
+				System.out.println("" + r1.getName() + " locked by Thread B");
+			}
+		}
+	}
 }
 
-public class Deadlock{
-  public static void main(String[] args) {
-    Resource r1 = new Resource("Resource1");
-    Resource r2 = new Resource("Resource2");
-    
-    ThreadA ta = new ThreadA(r1, r2);
-    Thread t1 = new Thread(ta);
-    ThreadB t2 = new ThreadB(r1, r2);
-    
-    t1.start();
-    
-    t2.start();
-  }
+public class Deadlock {
+	public static void main(String[] args) {
+		Resource r1 = new Resource("Resource1");
+		Resource r2 = new Resource("Resource2");
+
+		ThreadA ta = new ThreadA(r1, r2);
+		Thread t1 = new Thread(ta);
+		ThreadB t2 = new ThreadB(r1, r2);
+
+		t1.start();
+
+		t2.start();
+	}
 }
